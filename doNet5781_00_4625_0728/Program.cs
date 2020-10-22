9798d59a26c092ebb2276b38898eb159dda12c9f
Yocheved-Watenberg <ywatenbe@g.jct.ac.il>
2020-10-22 21:15:41 +0200

    Initial code + Welcome4625

---
+++ doNet5781_00_4625_0728/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace doNet5781_00_4625_0728
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
