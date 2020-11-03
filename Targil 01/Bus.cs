4f163d4631591d683dcfe7e73c3c04d79e591690
Yocheved-Watenberg <73307725+Yocheved-Watenberg@users.noreply.github.com>
2020-11-03 22:16:34 +0100

    Ex01 final commit

--- Targil 01/Bus.cs
+++ Targil 01/Bus.cs
@@ -8,18 +8,17 @@ namespace Targil_01
 {
     class Bus
     {
-
-        public const int GASOLINE = 1200;                                    //max of gazoline remaining after refueling
-        public const int MILEAGE = 20000;                                    //max of mileage remaining before the next overhaul
+        public const int GASOLINE = 1200;                                   //max of gazoline remaining after refueling
+        public const int MILEAGE = 20000;                                   //max of mileage remaining before the next overhaul
 
         public string BusNumber { get; set; }
         public DateTime FirstActivity { get; set; }
         public DateTime LastOverhaul { get; set; }
-        public int Gasoline { get; set; }                                  //amount of gasoline remaining   
+        public int Gasoline { get; set; }                                   //amount of gasoline remaining   
         public long TotalMileage { get; set; }                              //num of km that the bus travelled since its first activity
         public int Mileage { get; set; }                                    //number of km that the bus can travel before the next overhaul 
 
-        public Bus() { }                                                     //empty ctor 
+        public Bus() { }                                                    //empty ctor 
 
 
         public bool gasolineVerification(int km)                            //verify if the bus can travel this num of km before the next refueling
@@ -29,7 +28,7 @@ namespace Targil_01
                 Console.WriteLine("The bus can't travel,you have to put gasoline");
                 return false;
             }
-            return true;                                                     //the function return true if the bus can travel whith its amount of gasoline
+            return true;                                                    //the function return true if the bus can travel whith its amount of gasoline
         }
 
 
@@ -40,7 +39,7 @@ namespace Targil_01
                 Console.WriteLine("The bus needs overhaul beause the bus will travel more than 20,000 km since the last overhaul");
                 return false;
             }
-            if (AYearPassed(LastOverhaul, DateTime.Now))                   //check that it is less than a year since the last overhaul
+            if (AYearPassed(LastOverhaul, DateTime.Now))                    //check that it is less than a year since the last overhaul
             {
                 Console.WriteLine("The bus needs overhaul because a year passed since the last overhaul");
                 return false;
