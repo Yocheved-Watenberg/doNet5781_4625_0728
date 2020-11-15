4847dc6022b225a53285e2a0cfeb903bd3e4f16f
Yocheved-Watenberg <ywatenbe@g.jct.ac.il>
2020-11-15 10:38:33 +0100

    start of ex 2

---
+++ dotNet5781_02_4625_0728/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet5781_02_4625_0728
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BusStation b = new BusStation(66551, 22, 24);
+            Console.WriteLine(b.id);
+            Console.WriteLine(b);
+            Console.ReadLine();
+        }
+    }
+}
