b91e5a10cb1f7b9ccd009a52db24fa845b8c0fa4
Yocheved-Watenberg <ywatenbe@g.jct.ac.il>
2021-02-18 19:33:52 +0200

    fichiers xml a jour

--- Project/DAL/DO/Line.cs
+++ Project/DAL/DO/Line.cs
@@ -4,9 +4,9 @@
     {
         public int Id { get; set; }                                         //pour savoir s il est a jeru ou a tel aviv(rats automati)
         public int Code { get; set; }                                       //line number
-        public Enums.Areas Area { get; set; }                                    //in which area the bus travel
-        public int FirstStation { get; set; }                              //name of the first station
-        public double LastStation { get; set; }                         //name of the last station
+        public Enums.Areas Area { get; set; }                              //in which area the bus travel
+        public int FirstStation { get; set; }                              //code of the first station
+        public int LastStation { get; set; }                              //code of the last station
         public bool IsDeleted { get; set; } = false;
         public override string ToString()                                                //override ToString for a station
         {
