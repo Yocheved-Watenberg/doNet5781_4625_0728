f81ad3d8c632ff7ef086a0e8ea8d6a7c61f4adb4
Yocheved-Watenberg <ywatenbe@g.jct.ac.il>
2021-02-22 11:33:38 +0200

    erreur irrecuperable

--- Project/DAL/DO/LineTrip.cs
+++ Project/DAL/DO/LineTrip.cs
@@ -4,7 +4,7 @@ namespace DO
 {
     public class LineTrip
     {
-        // public int Id { get; set; }  c'est quoi ? 
+        public int Id { get; set; }                      //mispar ratz 
         public int LineId { get; set; }                  //line number(first entity key)
         public TimeSpan StartAt { get; set; }            //departure time(second entity key)
         public TimeSpan Frequency { get; set; }          //how often the bus goes out to the line?(if 0 one travel only)
